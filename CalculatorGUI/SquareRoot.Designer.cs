462f4cc50ff416df20d8835eebdf8a4de56ab193
doom-mi <dominicver@hotmail.com>
2022-04-14 18:37:53 -0400

    Added Square Root Form and Code
    Added Square Root form and code

---
+++ CalculatorGUI/SquareRoot.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace CalculatorGUI
+{
+    partial class SquareRoot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberDisplay = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.back = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.solve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberDisplay
+            // 
+            this.numberDisplay.AutoSize = true;
+            this.numberDisplay.Location = new System.Drawing.Point(94, 65);
+            this.numberDisplay.Name = "numberDisplay";
+            this.numberDisplay.Size = new System.Drawing.Size(0, 13);
+            this.numberDisplay.TabIndex = 29;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(33, 65);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(40, 13);
+            this.result.TabIndex = 28;
+            this.result.Text = "Result:";
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(19, 27);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(72, 13);
+            this.numberLabel.TabIndex = 27;
+            this.numberLabel.Text = "Enter Number";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(97, 24);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberTextBox.TabIndex = 26;
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(292, 89);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 25;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(292, 60);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 24;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // solve
+            // 
+            this.solve.Location = new System.Drawing.Point(292, 21);
+            this.solve.Name = "solve";
+            this.solve.Size = new System.Drawing.Size(75, 23);
+            this.solve.TabIndex = 23;
+            this.solve.Text = "Solve";
+            this.solve.UseVisualStyleBackColor = true;
+            this.solve.Click += new System.EventHandler(this.solve_Click);
+            // 
+            // SquareRoot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 147);
+            this.Controls.Add(this.numberDisplay);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.solve);
+            this.Name = "SquareRoot";
+            this.Text = "Square Root";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberDisplay;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button solve;
+    }
+}
---
+++ CalculatorGUI/SquareRoot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalculatorGUI
+{
+    public partial class SquareRoot : Form
+    {
+        public SquareRoot()
+        {
+            InitializeComponent();
+        }
+
+        private void solve_Click(object sender, EventArgs e)
+        {
+            double number7 = double.Parse(numberTextBox.Text);
+            numberDisplay.Text = string.Format("Square root of {0} is {1}.", number7, Math.Sqrt(number7));
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            numberDisplay.Text = "";
+            numberTextBox.Text = "";
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            CalculatorGUIForm calculatorGUI = new CalculatorGUIForm();
+            Hide();
+            calculatorGUI.ShowDialog();
+            Close();
+        }
+    }
+}
